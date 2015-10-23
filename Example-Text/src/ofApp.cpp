a801ba4bbb73126e09f723af59f2ae096eaa05b5
NickHardeman <nickhardeman@gmail.com>
2015-10-23 15:50:03 -0400

    text example

---
+++ Example-Text/src/ofApp.cpp
@@ -0,0 +1,77 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+    
+    // some system fonts like Helvetica on osx are contained in .dfont files or ttc files
+    // these types of files may contain several variations of the font face like regular, bold, italic, etc.
+    // OF uses the first instance in the file, which will probably not match the font variation used in the svg file
+    // you can place a fonts folder in the same directory as the svg file you are loading.
+    // Inside this folder place ttf fonts that should be used instead of the system font.
+    // For example, place a Helvetica.ttf file in the fonts folder if you are using Helvetica in your svg file.
+    // This will override the system Helvetica font which is most likely a ttc or dfont.
+    // To check the name of the fonts that are being used, open the .svg file in a text editor and check the
+    // font-family tags. 
+    
+    ofSetFrameRate( 60 );
+    svg.load( "sample.svg" );
+    cout << svg.toString();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    svg.draw();
+    ofSetColor( 255, 0, 0 );
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if( key == 'r' ) {
+        svg.reload();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
